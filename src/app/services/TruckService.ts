52e3c6393cd47e5372aa88eecfdf98d30223706e
Érica Lopes <1201474@isep.ipp.pt>
2022-11-27 21:12:11 +0000

    LP-34 Implementation of the validTruck() method #time 1h

--- src/app/services/TruckService.ts
+++ src/app/services/TruckService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
+import {MessageService} from "../message.service";
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,35 @@ import { map, Observable } from 'rxjs';
 export class TruckService {
   private Url = 'http://localhost:3000/api/truck/';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private messageService: MessageService) { }
 
-  createTruck(licensePlate: string ,autonomy:number, capacityCargo:number,capacityTransportation:number,battery: number, tare: number) {
-    const body={"licensePlate":licensePlate, "autonomy":autonomy, "capacityCargo":capacityCargo, "capacityTransportation":capacityTransportation, "battery": battery, "tare":tare};
-    return this.httpClient.post(this.Url,body)
-      .pipe(map(this.extractData)
-      );
+
+  log(message:string){
+    this.messageService.add(`Created Truck: ${message}`);
+  }
+
+  createValidTruck(licensePlate: string ,autonomy:number, capacityCargo:number,capacityTransportation:number,battery: number, tare: number){
+    if (this.validateData(licensePlate,autonomy,capacityCargo,capacityTransportation,battery,tare)){
+      this.createTruck(licensePlate,autonomy,capacityCargo,capacityTransportation,battery,tare);
+    }
+      else return this.log("ERROR while creating the truck");
+  }
+
+
+  createTruck(licensePlate: string ,autonomy:number, capacityCargo:number,capacityTransportation:number,battery: number, tare: number)  {
+   // if (this.validateData(licensePlate, autonomy, capacityCargo, capacityTransportation, battery, tare)) {
+      const body = {
+        "licensePlate": licensePlate,
+        "autonomy": autonomy,
+        "capacityCargo": capacityCargo,
+        "capacityTransportation": capacityTransportation,
+        "battery": battery,
+        "tare": tare
+      };
+      return this.httpClient.post(this.Url, body)
+        .pipe(map(this.extractData)
+        );
+    //} else return this.log("ERROR while creating the truck")
   }
 
   public extractData(res: any) {
@@ -25,6 +48,11 @@ export class TruckService {
     return this.httpClient.get(this.Url + 'all').pipe(
       map(this.extractData));
   }
+
+ /* getTruckByIdentifier(licensePlate: string): Observable<any> {
+    return this.httpClient.get(this.Url + '?ls=' + licensePlate).pipe(map(this.extractData));
+  }
+*/
 
   listTable(): void {
 
@@ -49,6 +77,52 @@ export class TruckService {
         td_battery.innerText = i[j].battery;
         td_tare.innerText = i[j].tare;
       }
+
+
     });
   }
+
+
+  validateData(licensePlate: string ,autonomy:number, capacityCargo:number,capacityTransportation:number,battery: number, tare: number):boolean {
+
+
+    let flag: boolean=true;
+
+    if (licensePlate.length!=8) {
+      this.log("ERROR: Please insert a valid License Plate");
+      flag=false;
+    }
+
+   /* if (this.getTruckByIdentifier(licensePlate)!=null){
+      this.log("ERROR: Truck already exists with that licensePlate");
+      flag=false;
+    }
+*/
+
+    if (autonomy<=0) {
+      this.log("ERROR: Autonomy must be positive");
+      flag=false;
+    }
+
+    if (capacityCargo<=0){
+      this.log("ERROR: Capacity Cargo must be positive");
+    }
+
+    if (capacityTransportation<=0){
+      this.log("ERROR: Capacity Transportation must be positive");
+    }
+
+    if (battery<=0){
+      this.log("ERROR: Battery must be positive");
+    }
+
+    if (tare<=0){
+      this.log("ERROR: Tare must be positive");
+    }
+
+    return flag;
+
+
+  }
+
 }

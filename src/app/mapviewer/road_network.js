cb7aced6c177cd5b86e4a1fc6dd7d95afcebd18e
Hugo Carvalho <1210813@isep.ipp.pt>
2022-11-27 00:16:02 +0000

    LP-51 Created the graph with axis and helper lines

--- src/app/mapviewer/road_network.js
+++ src/app/mapviewer/road_network.js
@@ -63,6 +63,70 @@ export default class RoadNetwork {
         mesh.position.y = -0.2;
 
         this.scene.add(mesh);
+
+        ///////////////////////////
+        // Draw solid axis lines //
+        ///////////////////////////
+        this.drawSolidLines([
+            new THREE.Vector3(0, 0, 70),
+            new THREE.Vector3(0, 0, -70),
+            new THREE.Vector3(70, 0, 0),
+            new THREE.Vector3(-70, 0, 0)
+        ], new THREE.LineBasicMaterial({ color: 0x000000 }));
+
+        //////////////////////////////
+        // Draw dashed helper lines //
+        //////////////////////////////
+        this.drawDottedLines([
+            [new THREE.Vector3(-60, 0, 39.5), new THREE.Vector3(-60, 0, -40)],
+            [new THREE.Vector3(-50, 0, 49.5), new THREE.Vector3(-50, 0, -50)],
+            [new THREE.Vector3(-40, 0, 59.5), new THREE.Vector3(-40, 0, -60)],
+            [new THREE.Vector3(-30, 0, 59.5), new THREE.Vector3(-30, 0, -60)],
+            [new THREE.Vector3(-20, 0, 69.5), new THREE.Vector3(-20, 0, -70)],
+            [new THREE.Vector3(-10, 0, 69.5), new THREE.Vector3(-10, 0, -70)],
+            [new THREE.Vector3(60, 0, 39.5), new THREE.Vector3(60, 0, -40)],
+            [new THREE.Vector3(50, 0, 49.5), new THREE.Vector3(50, 0, -50)],
+            [new THREE.Vector3(40, 0, 59.5), new THREE.Vector3(40, 0, -60)],
+            [new THREE.Vector3(30, 0, 59.5), new THREE.Vector3(30, 0, -60)],
+            [new THREE.Vector3(20, 0, 69.5), new THREE.Vector3(20, 0, -70)],
+            [new THREE.Vector3(10, 0, 69.5), new THREE.Vector3(10, 0, -70)],
+            [new THREE.Vector3(39.5, 0, -60), new THREE.Vector3(-40, 0, -60)],
+            [new THREE.Vector3(49.5, 0, -50), new THREE.Vector3(-50, 0, -50)],
+            [new THREE.Vector3(59.5, 0, -40), new THREE.Vector3(-60, 0, -40)],
+            [new THREE.Vector3(59.5, 0, -30), new THREE.Vector3(-60, 0, -30)],
+            [new THREE.Vector3(69.5, 0, -20), new THREE.Vector3(-70, 0, -20)],
+            [new THREE.Vector3(69.5, 0, -10), new THREE.Vector3(-70, 0, -10)],
+            [new THREE.Vector3(39.5, 0, 60), new THREE.Vector3(-40, 0, 60)],
+            [new THREE.Vector3(49.5, 0, 50), new THREE.Vector3(-50, 0, 50)],
+            [new THREE.Vector3(59.5, 0, 40), new THREE.Vector3(-60, 0, 40)],
+            [new THREE.Vector3(59.5, 0, 30), new THREE.Vector3(-60, 0, 30)],
+            [new THREE.Vector3(69.5, 0, 20), new THREE.Vector3(-70, 0, 20)],
+            [new THREE.Vector3(69.5, 0, 10), new THREE.Vector3(-70, 0, 10)]
+        ], new THREE.LineDashedMaterial({
+            color: 0xa0a0a0,
+            dashSize: 4,
+            gapSize: 1
+        }));
+    }
+
+    // Draws solid lines
+    drawSolidLines(points, material) {
+
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        this.scene.add(new THREE.LineSegments(geometry, material));
+    }
+
+    // Draws dotted lines
+    drawDottedLines(points, material) {
+
+        let geometry, line;
+
+        for (let i = 0; i < points.length; i++) {
+            geometry = new THREE.BufferGeometry().setFromPoints(points[i]);
+            line = new THREE.LineSegments(geometry, material);
+            line.computeLineDistances();
+            this.scene.add(line);
+        }
     }
 
     // Updates the camera aspect ratio and the renderer size when the window is resized

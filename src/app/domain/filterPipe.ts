1c9a16ecb4ec1b290eb18be36d8483c796027e78
Hugo Carvalho <1210813@isep.ipp.pt>
2022-12-02 16:42:17 +0000

    LP-70 Updated the filter method to handle more domain classes

--- src/app/domain/filterPipe.ts
+++ src/app/domain/filterPipe.ts
@@ -7,52 +7,108 @@ import {Injectable, Pipe, PipeTransform} from '@angular/core';
 @Injectable()
 export class FilterPipe implements PipeTransform {
 
-  transform(items: any[], licensePlate: string, autonomy: string, capacityCargo: string, capacityTransportation: string, battery: string, tare: string): any[] {
+  transform(items: any[], domainType: string, params: any): any[] {
 
     if (!items) {
       return [];
     }
 
-    if (!licensePlate && !autonomy && !capacityCargo && !capacityTransportation && !battery && !tare) {
-      return items;
-    }
-
     let finalList = JSON.parse(JSON.stringify(items)) as any[];
 
-    if (licensePlate) {
-      finalList = finalList.filter((singleItem) =>
-        singleItem['licensePlate'].toLowerCase().includes(licensePlate.toLowerCase())
-      );
-    }
+    if (domainType === "delivery") {
 
-    if (autonomy) {
-      finalList = finalList.filter((singleItem) =>
-        singleItem['autonomy'].toString().toLowerCase().includes(autonomy.toLowerCase())
-      );
-    }
+      //TODO
+    } else if (domainType === "route") {
 
-    if (capacityCargo) {
-      finalList = finalList.filter((singleItem) =>
-        singleItem['capacityCargo'].toString().toLowerCase().includes(capacityCargo.toLowerCase())
-      );
-    }
+      if (!params.routeId && !params.origin && !params.destination && !params.distance && !params.timeDistance && !params.energySpent && !params.extraBatteryTime) {
+        return items;
+      }
 
-    if (capacityTransportation) {
-      finalList = finalList.filter((singleItem) =>
-        singleItem['capacityTransportation'].toString().toLowerCase().includes(capacityTransportation.toLowerCase())
-      );
-    }
+      if (params.routeId) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['routeId'].toLowerCase().includes(params.routeId.toLowerCase())
+        );
+      }
 
-    if (battery) {
-      finalList = finalList.filter((singleItem) =>
-        singleItem['battery'].toString().toLowerCase().includes(battery.toLowerCase())
-      );
-    }
+      if (params.origin) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['origin'].toLowerCase().includes(params.origin.toLowerCase())
+        );
+      }
 
-    if (tare) {
-      finalList = finalList.filter((singleItem) =>
-        singleItem['tare'].toString().toLowerCase().includes(tare.toLowerCase())
-      );
+      if (params.destination) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['destination'].toLowerCase().includes(params.destination.toLowerCase())
+        );
+      }
+
+      if (params.distance) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['distance'].toString().toLowerCase().includes(params.distance.toLowerCase())
+        );
+      }
+
+      if (params.timeDistance) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['timeDistance'].toString().toLowerCase().includes(params.timeDistance.toLowerCase())
+        );
+      }
+
+      if (params.energySpent) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['energySpent'].toString().toLowerCase().includes(params.energySpent.toLowerCase())
+        );
+      }
+
+      if (params.extraBatteryTime) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['extraBatteryTime'].toString().toLowerCase().includes(params.extraBatteryTime.toLowerCase())
+        );
+      }
+    } else if (domainType === "truck") {
+
+      if (!params.licensePlate && !params.autonomy && !params.capacityCargo && !params.capacityTransportation && !params.battery && !params.tare) {
+        return items;
+      }
+
+      if (params.licensePlate) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['licensePlate'].toLowerCase().includes(params.licensePlate.toLowerCase())
+        );
+      }
+
+      if (params.autonomy) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['autonomy'].toString().toLowerCase().includes(params.autonomy.toLowerCase())
+        );
+      }
+
+      if (params.capacityCargo) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['capacityCargo'].toString().toLowerCase().includes(params.capacityCargo.toLowerCase())
+        );
+      }
+
+      if (params.capacityTransportation) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['capacityTransportation'].toString().toLowerCase().includes(params.capacityTransportation.toLowerCase())
+        );
+      }
+
+      if (params.battery) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['battery'].toString().toLowerCase().includes(params.battery.toLowerCase())
+        );
+      }
+
+      if (params.tare) {
+        finalList = finalList.filter((singleItem) =>
+          singleItem['tare'].toString().toLowerCase().includes(params.tare.toLowerCase())
+        );
+      }
+    } else if (domainType === "warehouse") {
+
+      //TODO
     }
 
     return finalList;

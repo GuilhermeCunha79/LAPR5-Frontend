f81d99455a90885570b3bd0e327255e991bc9511
Hugo Carvalho <1210813@isep.ipp.pt>
2022-12-28 19:29:58 +0000

    LP-83 (Implementation) Added a new dependency for social media logins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {SharedModule} from "./shared.module";
+import {GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from '@abacritt/angularx-social-login';
 
 import {AppComponent} from './app.component';
-import {CreateDeliveryComponent} from './create-delivery/create-delivery.component';
 import {MessagesComponent} from './messages/messages.component';
-import {CreateTruckComponent} from './create-truck/create-truck.component';
-import {HomepageComponent} from './homepage/homepage.component';
-import {CreateWarehouseComponent} from './create-warehouse/create-warehouse.component';
-import {CreateRouteComponent} from './create-route/create-route.component';
-import {SharedModule} from "./shared.module";
-import {CreatePlanningComponent} from './create-planning/create-planning.component';
-import {RoadNetworkComponent} from "./road-network/road-network.component";
-import {LoginScreenComponent} from './login-screen/login-screen.component';
 import {DefaultLayoutComponent} from './default-layout/default-layout.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {LoginScreenComponent} from './login-screen/login-screen.component';
+import {CreateDeliveryComponent} from './create-delivery/create-delivery.component';
+import {CreatePlanningComponent} from './create-planning/create-planning.component';
+import {CreateRouteComponent} from './create-route/create-route.component';
+import {CreateTruckComponent} from './create-truck/create-truck.component';
+import {CreateWarehouseComponent} from './create-warehouse/create-warehouse.component';
+import {RoadNetworkComponent} from "./road-network/road-network.component";
 
 @NgModule({
   imports: [
@@ -23,25 +24,36 @@ import {DefaultLayoutComponent} from './default-layout/default-layout.component'
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    SocialLoginModule
   ],
-
   declarations: [
     AppComponent,
     MessagesComponent,
     DefaultLayoutComponent,
-
     HomepageComponent,
     LoginScreenComponent,
-
     CreateDeliveryComponent,
+    CreatePlanningComponent,
+    CreateRouteComponent,
     CreateTruckComponent,
     CreateWarehouseComponent,
-    CreateRouteComponent,
-    CreatePlanningComponent,
     RoadNetworkComponent
   ],
-
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('130051265539-iqqg4sl2l1bum9t6ketuop7llr0tbltt.apps.googleusercontent.com')
+          }
+        ], onError: (err) => console.error(err)
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 

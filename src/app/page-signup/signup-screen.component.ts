c2324ff72660621c148c557f8dce3725d1c039e9
Hugo Carvalho <1210813@isep.ipp.pt>
2022-12-31 03:10:06 +0000

    LP-83 (Implementation) Implemented the signup option

--- src/app/page-signup/signup-screen.component.ts
+++ src/app/page-signup/signup-screen.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth/auth.service";
 
 @Component({
   selector: 'app-signup-screen',
   templateUrl: './signup-screen.component.html',
   styleUrls: ['./signup-screen.component.css']
 })
+
 export class SignupScreenComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  role: number = 1;
 
-  ngOnInit(): void {
+  constructor(private signupService: AuthService) {
   }
 
+  ngOnInit(): void {
+    this.signupService.redirectToHome();
+  }
+
+  signup(): void {
+    this.signupService.createValidUser(this.email, this.password, this.firstName, this.lastName, this.phoneNumber, this.role)?.subscribe(obj => {
+      sessionStorage.setItem("user-data", JSON.stringify(obj));
+      this.signupService.redirectToHome();
+    });
+  }
 }

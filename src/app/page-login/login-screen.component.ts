00a46b1a87fb9106890908202dbb075bfee25cec
Hugo Carvalho <1210813@isep.ipp.pt>
2022-12-31 03:09:47 +0000

    LP-83 (Implementation) Added the login functionality

--- src/app/page-login/login-screen.component.ts
+++ src/app/page-login/login-screen.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth/auth.service";
 
 @Component({
   selector: 'app-login-screen',
@@ -11,15 +12,20 @@ export class LoginScreenComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor() {
+  constructor(private signupService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.signupService.redirectToHome();
   }
 
   login(): void {
-  }
-
-  signup(): void {
+    if (this.signupService.login(this.email, this.password)) {
+      this.signupService.getUser(this.email, this.password)?.subscribe(obj => {
+        console.log(obj)
+        sessionStorage.setItem("user-data", JSON.stringify(obj));
+        this.signupService.redirectToHome();
+      });
+    }
   }
 }

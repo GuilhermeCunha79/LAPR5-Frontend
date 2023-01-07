ea1b1bad546915b196dc64e959e57b8008ab7cf2
Hugo Carvalho <1210813@isep.ipp.pt>
2023-01-07 01:28:58 +0000

    LP-94 (Implementation) Updated the signup page to handle Google account signup

--- src/app/page-signup/page-signup.component.ts
+++ src/app/page-signup/page-signup.component.ts
@@ -16,11 +16,24 @@ export class PageSignupComponent implements OnInit {
   phoneNumber: string;
   role: number = 1;
 
+  blockInput: boolean = false;
+  blockSignup: boolean = false;
+
   constructor(private signupService: AuthService) {
   }
 
   ngOnInit(): void {
     this.signupService.redirectToHome();
+
+    const googleUser = JSON.parse(sessionStorage.getItem("google-user")!);
+    if (googleUser) {
+      this.blockInput = true;
+      this.email = googleUser.email;
+      this.password = "0000000000000000"
+      this.firstName = googleUser.firstName;
+      this.lastName = googleUser.lastName;
+      this.phoneNumber = "939347107";
+    }
   }
 
   signup(): void {
@@ -28,5 +41,10 @@ export class PageSignupComponent implements OnInit {
       sessionStorage.setItem("user-data", JSON.stringify(obj.userDTO));
       this.signupService.redirectToHome();
     });
+  }
+
+  returnToLogin(): void {
+    sessionStorage.removeItem("google-user");
+    this.signupService.redirect('/login');
   }
 }
